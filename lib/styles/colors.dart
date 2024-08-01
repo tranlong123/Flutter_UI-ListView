b850f1b728b599325a160d5da5afe1f229619fc7
Tran Long <tranlonghazzi@gmail.com>
2024-08-01 14:43:09 +0700

    Revert "Home screen and movie list screen"
    This reverts commit 7812cd8d91661959c0f09ba92ab0906612d35cdb.

--- lib/styles/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const bgColor = Color(0xFF716850);
-const selectedItemColor = Color(0xFFF36464);
-const unselectedItemColor = Color(0xFFB9B9B9);
-const textInputBg = Color.fromRGBO(220, 220, 220, 1);
-const dotColor = Color(0xFFBD9E9E);
-const black = Colors.black;
